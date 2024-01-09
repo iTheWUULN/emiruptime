0fe5d83fe0fb46a36e11a7703305e48a44b1c24c
iTheWUULN <96634321+iTheWUULN@users.noreply.github.com>
2024-01-09 17:28:42 +0300

    Add files via upload

---
+++ views/emojis.js
@@ -0,0 +1,69 @@
+// @ts-check
+
+const Tik = "<:Tik:1194282762971136010>";
+const Çarpı = "<:Carpi:1194282776522928169>";
+const Link = "<:Link:1194284249285656727>";
+const Yasak = "<:Yasak:1194282694796922910>";
+const Bakım = "<:Bakim:1194282405234757692>";
+const Kullanıcı = "<:Kullanici:1194284702194991144>";
+const İsim = "<:Kullanici:1194284702194991144>";
+const Id = "<:Id:1047166052741697587>";
+const Belge = "<:Belge:1046825193131225169>";
+const Premium = "<:boost:1194282415384952873>";
+const Yazı = "<:Yazi:1048677751818821702>";
+const Bot = "<:Bot:1051374431819284603>";
+const Duyuru = "<:Duyuru:1194282402621685814>";
+const Davet = "<:Davet:1047160005998166056>";
+const Çöp = "<:Cop:1066000658190311424>";
+const Saat = "<:Saat:1065251905497993347>";
+const Ping = "<:Ping:1065675122683412481>";
+const Limit = "<:Limit:1065321210847707227>";
+const Karaliste = "<:Karaliste:1047167116727550023>";
+const Yetkili = "<:Yetkili:1047167457703497728>";
+const Sebep = "<:Sebep:1047168561392660602>";
+const Tac = "<:Tac:1046822690448748574>";
+const Js = "<:Js:1046823440688103434>";
+const Node = "<:Node:1046823461290508338>";
+const Uptime = "<:Uptime:1046823485428731934>";
+const Sunucu = "<:Sunucu:1046824609758060624>";
+const Ram = "<:Ram:1069551472544587806>";
+const Giriş = "<:aktif:1194282630787641454>";
+const Çıkış = "<:kapal:1194282636416385054>";
+const Hata = "<:yasak:1194282694796922910>";
+const Destek = "<:Destek:1194285215850434595>";
+const Oy = "<:Oy:1067128355830366228>";
+
+module.exports = {
+  Tik,
+  Çarpı,
+  Link,
+  Yasak,
+  Bakım,
+  Kullanıcı,
+  İsim,
+  Id,
+  Belge,
+  Premium,
+  Yazı,
+  Bot,
+  Duyuru,
+  Davet,
+  Çöp,
+  Saat,
+  Ping,
+  Limit,
+  Karaliste,
+  Yetkili,
+  Sebep,
+  Tac,
+  Js,
+  Node,
+  Uptime,
+  Sunucu,
+  Ram,
+  Giriş,
+  Çıkış,
+  Hata,
+  Destek,
+  Oy,
+};
